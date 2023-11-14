400c06696bd16eec3f5c2f7efdccde43e179f1db
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-11-14 22:42:13 +0300

    delete onClick

--- sign-up/src/Login.js
+++ sign-up/src/Login.js
@@ -38,7 +38,6 @@ const Login = () => {
       </label>
       <button
         className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
-        onClick={handleLogin}
       >
         Login
       </button>
--- sign-up/src/Sup.js
+++ sign-up/src/Sup.js
@@ -60,7 +60,7 @@ const SignUp = () => {
       </label>
       <button
         className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
-        onClick={handleSignUp}
+    
       >
         Sign Up
       </button>
